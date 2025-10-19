9687d4b9946dd4054c67b1249685edf46dca607d
AnastasiaPelyno <anastasiia.pelyno@lnu.edu.ua>
2025-10-19 20:35:31 +0300

    Add setup.py for proper tail-cli installation

--- setup.py
+++ setup.py
@@ -1,15 +1,21 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='tail-cli',
-    version='1.0',
-    author='Anastasia Pelyno',
-    description='CLI утиліта tail на Python',
-    packages=find_packages(),
-    install_requires=['click', 'colorama'],
+    name="tail-cli",
+    version="1.0",
+    packages=find_packages(),  # автоматично знайде папку tail_cli
+    include_package_data=True,
+    install_requires=[
+        "click",
+        "colorama"
+    ],
     entry_points={
-        'console_scripts': [
-            'tail-cli=tail_cli.tail:tail',
-        ],
+        "console_scripts": [
+            "tail-cli = tail_cli.tail:tail"  # команда tail-cli викликає функцію tail() у tail.py
+        ]
     },
+    python_requires=">=3.7",
+    description="Tail CLI — програма tail на Python з підтримкою Click і кольорового виводу.",
+    author="Anastasia Pelyno",
+    license="MIT",
 )
