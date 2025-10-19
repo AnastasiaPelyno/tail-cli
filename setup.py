bb291cf2e9d21b50cd4a9f7d7e81973b62904538
AnastasiaPelyno <anastasiia.pelyno@lnu.edu.ua>
2025-10-19 21:53:45 +0300

    Fix package

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="tail-cli",
-    version="1.0",
+    version="1.0.1",
     packages=find_packages(),  # автоматично знайде папку tail_cli
     include_package_data=True,
     install_requires=[
