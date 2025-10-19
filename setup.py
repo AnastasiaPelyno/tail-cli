98a597429ed56487d05500374190b9b7426d9e76
AnastasiaPelyno <anastasiia.pelyno@lnu.edu.ua>
2025-10-19 23:52:43 +0300

    Update

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="tail-cli",
-    version="1.0.1",
+    version="1.0.2",
     packages=find_packages(),  # автоматично знайде папку tail_cli
     include_package_data=True,
     install_requires=[
